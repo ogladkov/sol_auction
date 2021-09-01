b4c2a7b1f3a8cf3642da8b4416ff6c12fe0aadb0
ogladkov <fg404@me.com>
2021-09-01 13:24:27 +0300

    Added code

---
+++ solidity_auction.sol
@@ -0,0 +1,70 @@
+pragma solidity ^0.7.0;
+
+
+contract simpleAuction{
+    address payable public beneficiary;
+    uint public auctionEndTime;
+
+    address public highestBidder;
+    uint public highestBid;
+
+    mapping(address => uint) public pendingReturns;
+
+    bool ended = false;
+
+    event HighestBidIncrease(address bidder, uint amount);
+    event AuctionEnded(address winner, uint amount);
+
+    constructor(uint _biddingTime, address payable _beneficiary){
+        beneficiary = _beneficiary;
+        auctionEndTime = block.timestamp + _biddingTime;
+    }
+
+    function bid() public payable{
+        if (block.timestamp > auctionEndTime){
+            revert("The auction has already ended");
+        }
+
+        if (msg.value <= highestBid){
+            revert("There is already a highter or equal bid");
+        }
+
+        if (highestBid != 0){
+            pendingReturns[highestBidder] += highestBid;
+        }
+
+        highestBidder = msg.sender;
+        highestBid = msg.value;
+        emit HighestBidIncrease(msg.sender, msg.value);
+    }
+
+    function withdraw() public returns(bool){
+        uint amount = pendingReturns[msg.sender];
+        if (amount > 0){
+            pendingReturns[msg.sender] = 0;
+
+            if (!payable(msg.sender).send(amount)){
+                pendingReturns[msg.sender] = amount;
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    function auctionEnd() public {
+        if (block.timestamp < auctionEndTime){
+            revert("The auction has not ended yet");
+        }
+
+        if (ended) {
+            revert("The function auctionEnded has already been called");
+        }
+
+        ended = true;
+        emit AuctionEnded(highestBidder, highestBid);
+
+        beneficiary.transfer(highestBid);
+    }
+
+}
